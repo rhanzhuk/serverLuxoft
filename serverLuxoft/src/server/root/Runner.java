ff7e652b2cd753fe181ff422c2669001f6ef9287
DSK15 DSK <DSK15@ptc.local>
2018-10-11 18:00:48 +0300

    todo server

--- serverLuxoft/src/server/root/Runner.java
+++ serverLuxoft/src/server/root/Runner.java
@@ -4,5 +4,18 @@ public class Runner {
 
     public static void main(String[] args) {
 
+
+
     }
 }
+
+/*
+public class ServerTest {
+    public static void main(String[] args) {
+//        Server server = new Server();
+//        server.setWebAppPath("resources/webapp");
+//        server.setPort(3000);
+//        server.start();
+    }
+}
+ */
--- serverLuxoft/src/server/root/Server.java
+++ serverLuxoft/src/server/root/Server.java
@@ -1,4 +1,6 @@
 package server.root;
 
 public class Server {
+    //TODO like https://gist.github.com/Rooman/ccf78918f884ae354580cf9ebd70142b
+    //private
 }
